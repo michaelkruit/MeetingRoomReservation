c093316c80ff67d0f6324bdba521b6b879db6f27
Michael Kruit <michael_mikin@hotmail.com>
2021-10-06 20:07:46 +0200

    Add accountcontroller skeleton

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingRooms.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(object loginModel) 
+        {
+            // TODO: Login user
+
+            // After login is success, redirect user to dashboard
+            return RedirectToAction();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(object registerModel)
+        {
+            // TODO: Register new user
+
+            // After register is success, redirect user to dashboard
+            return RedirectToAction();
+        }
+    }
+}
