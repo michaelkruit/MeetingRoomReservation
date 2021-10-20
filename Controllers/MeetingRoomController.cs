c36066d7775cf8c3b1492d53e5dd31af074e177c
Michael Kruit <michael_mikin@hotmail.com>
2021-10-20 22:17:26 +0200

    Implement Index, Details and Create controller logic for MeetingRooms

--- Controllers/MeetingRoomController.cs
+++ Controllers/MeetingRoomController.cs
@@ -1,23 +1,67 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MeetingRooms.Interfaces;
+using MeetingRooms.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MeetingRooms.Controllers
 {
-    [Authorize]
     public class MeetingRoomController : Controller
     {
-        public MeetingRoomController(IMeetingRoomIN)
-        {
+        private readonly IMeetingRoomRepository _meetingRoomRepository;
 
+        public MeetingRoomController(IMeetingRoomRepository meetingRoomRepository)
+        {
+            _meetingRoomRepository = meetingRoomRepository;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var token = HttpContext.Session.GetString("Token");
+
+            var meetingRooms = await _meetingRoomRepository.GetList(token);
+
+            // Map to viewmodel
+            var meetingRoomsViewModel = meetingRooms.Select(x => new MeetingRoomViewModel()
+            {
+                CompanyId = x.CompanyId,
+                Id = x.Id,
+                Name = x.Name
+            });
+
+            return View(meetingRoomsViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var meetingRoom = await _meetingRoomRepository.GetSingle(id);
+
+            return View(meetingRoom);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MeetingRoomCreateViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var token = HttpContext.Session.GetString("Token");
+
+            var meetingRoom = await _meetingRoomRepository.Create(token, model);
+
+            return RedirectToAction(nameof(Details), new { meetingRoom.Id });
         }
     }
 }
