a2b32bf86c9606a8ec5df5f8f24d7c2d7a472341
Michael Kruit <michael_mikin@hotmail.com>
2021-10-25 21:50:36 +0200

    Create MeetingRepository interface

---
+++ Interfaces/IMeetingRepository.cs
@@ -0,0 +1,17 @@
+﻿using MeetingRooms.Data.Entities;
+using MeetingRooms.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MeetingRooms.Interfaces
+{
+    public interface IMeetingRepository
+    {
+        Task<IEnumerable<Meeting>> GetCompanyList(string token);
+        Task<IEnumerable<Meeting>> GetMeetingRoomList(string token, int meetingRoomId);
+        Task<Meeting> GetSingle(string token, int id);
+        Task<Meeting> Create(string token, MeetingCreateViewModel createModel);
+        Task<Meeting> Update(string token, MeetingUpdateViewModel updateModel);
+        Task<bool> Delete(string token, int id);
+    }
+}
