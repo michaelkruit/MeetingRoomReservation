7974108435feaee3ab4e2cb991e2bd2c3c0e84d5
Michael Kruit <michael_mikin@hotmail.com>
2021-10-25 21:50:18 +0200

    Create meeting ViewModels

---
+++ ViewModels/MeetingViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MeetingRooms.ViewModels
+{
+    public abstract class MeetingBaseViewModel
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string AttendingCompany { get; set; }
+        public int MeetingRoomId { get; set; }
+    }
+
+    public class MeetingCreateViewModel : MeetingBaseViewModel { }
+    
+    public class MeetingUpdateViewModel : MeetingBaseViewModel 
+    {
+        public int Id { get; set; }
+    }
+
+    public class MeetingViewModel : MeetingUpdateViewModel { }
+}
