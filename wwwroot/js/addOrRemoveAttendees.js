6c9ab5be84c17e9f5a224d734edad5452c783f7b
Michael Kruit <michael_mikin@hotmail.com>
2021-11-10 21:15:15 +0100

    Make it possible to add and remove attendees in the front-end

---
+++ wwwroot/js/addOrRemoveAttendees.js
@@ -0,0 +1,19 @@
+﻿const attendeeBtn = document.getElementById('add-attendee-btn');
+const attendeesContainter = document.getElementById('attendees-container');
+
+attendeeBtn.addEventListener('click', addAttendee);
+
+function addAttendee() {
+    let div = document.createElement('div');
+    let newInput = `<div class="input-group mb-3">
+                        <input type="text" class="form-control" name="Attendees[]" value="" />
+                        <button type="button" class="btn btn-sm btn-danger" onclick="removeAttendee(this)">Remove</button>
+                    </div>`;
+    div.innerHTML = newInput;
+    attendeesContainter.appendChild(div);
+}
+
+function removeAttendee(elem) {
+    const divToRemove = elem.parentNode;
+    divToRemove.remove();
+}
